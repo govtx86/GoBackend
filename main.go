2cb60af4d75437ad5c711c3e53b704fb539d6d71
govtx86 <govindsanal08@gmail.com>
2025-06-26 18:53:54 +0530

    Add graceful shutdown

--- main.go
+++ main.go
@@ -1,13 +1,42 @@
 package main
 
 import (
+	"context"
 	"flag"
 	"fmt"
+	"log"
 	"net/http"
+	"os/signal"
+	"syscall"
 	"time"
 	"todoapp/internal/app"
 	"todoapp/internal/routes"
 )
+
+func gracefulShutdown(apiServer *http.Server, done chan bool) {
+	// Create context that listens for the interrupt signal from the OS.
+	ctx, stop := signal.NotifyContext(context.Background(), syscall.SIGINT, syscall.SIGTERM)
+	defer stop()
+
+	// Listen for the interrupt signal.
+	<-ctx.Done()
+
+	log.Println("shutting down gracefully, press Ctrl+C again to force")
+	stop() // Allow Ctrl+C to force shutdown
+
+	// The context is used to inform the server it has 5 seconds to finish
+	// the request it is currently handling
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+	if err := apiServer.Shutdown(ctx); err != nil {
+		log.Printf("Server forced to shutdown with error: %v", err)
+	}
+
+	log.Println("Server exiting")
+
+	// Notify the main goroutine that the shutdown is complete
+	done <- true
+}
 
 func main() {
 	var port int
@@ -18,16 +47,21 @@ func main() {
 	if err != nil {
 		panic(err)
 	}
-	// defer app.DB.Close()
 	r := routes.SetupRoutes(app)
 
 	server := &http.Server{
-		Addr: fmt.Sprintf(":%d", port),
-		Handler: r,
+		Addr:         fmt.Sprintf(":%d", port),
+		Handler:      r,
 		IdleTimeout:  time.Minute,
 		ReadTimeout:  10 * time.Second,
 		WriteTimeout: 30 * time.Second,
 	}
+
+	// Create a done channel to signal when the shutdown is complete
+	done := make(chan bool, 1)
+
+	// Run graceful shutdown in a separate goroutine
+	go gracefulShutdown(server, done)
 
 	app.Logger.Printf("Server is running on port: %d\n", port)
 
@@ -35,4 +69,8 @@ func main() {
 	if err != nil {
 		app.Logger.Fatal(err)
 	}
+
+	// Wait for the graceful shutdown to complete
+	<-done
+	log.Println("Graceful shutdown complete.")
 }
